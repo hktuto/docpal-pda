df7e0f65057faf1cd9cc4df9d373bdfe9349a575
Sean Tsang <9394wing@gmail.com>
2026-07-03 17:32:01 +0800

    feat: add StatusBadge component

---
+++ components/StatusBadge.vue
@@ -0,0 +1,13 @@
+<template>
+  <span class="badge" :class="badgeClass(status)">
+    <slot>{{ status }}</slot>
+  </span>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  status: string;
+}>();
+
+const { badgeClass } = useStatusBadge();
+</script>
