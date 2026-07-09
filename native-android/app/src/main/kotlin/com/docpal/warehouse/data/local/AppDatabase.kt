c5e5db6396e1298c4e06a2fc2d5b3134d3986592
Sean Tsang <9394wing@gmail.com>
2026-07-09 23:12:35 +0800

    feat(native): add AppDatabase

---
+++ native-android/app/src/main/kotlin/com/docpal/warehouse/data/local/AppDatabase.kt
@@ -0,0 +1,15 @@
+package com.docpal.warehouse.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.docpal.warehouse.data.local.dao.UserDao
+import com.docpal.warehouse.data.local.entity.UserEntity
+
+@Database(
+    entities = [UserEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun userDao(): UserDao
+}
